e13223c42b84d8c1914e559c32cd44493db638af
he <18913232512@163.com>
2021-06-04 15:31:15 +0800

    mini-games

---
+++ 2048/js/animation.js
@@ -0,0 +1,28 @@
+//通过动画显示数字
+function showNumberWihthAnimation(i, j, randNumber) {
+	//console.log(i,j,randNumber);
+	let numberCell = $('#number-cell-' + i + '-' + j); //#符号很容易被忽视 ID
+	numberCell.css({
+		'background-color': getNumberBackgroundColor(randNumber),
+		'color': getNumberColor(randNumber),
+		'font-size': getNumberFontSize(randNumber) + 'px'
+	}).text(randNumber);
+
+	numberCell.animate({
+		width: cellWidth,
+		height: cellWidth,
+		top: getPosTop(i, j),
+		left: getPosLeft(i, j)
+	}, 500);
+
+}
+
+//通过动画显示移动的效果
+function showMoveAnimation(fromx, fromy, tox, toy) {
+	var numberCell = $('#number-cell-' + fromx + '-' + fromy);
+	numberCell.animate({
+		top: getPosTop(tox, toy),
+		left: getPosLeft(tox, toy)
+	}, 200);
+
+}
